44b251208a4b46c2b060dac2e872e2fabf7bcf0c
JShankelModumate <63130365+JShankelModumate@users.noreply.github.com>
2020-04-06 18:44:49 -0700

    Update ModumateCrafting.cpp (#12)
    This restores a fix to crafting node pointers that was clobbered by Perforce integration of categorical node changes in ModumateCrafting.cpp

--- Modumate/Source/Modumate/Private/Database/ModumateCrafting.cpp
+++ Modumate/Source/Modumate/Private/Database/ModumateCrafting.cpp
@@ -791,7 +791,7 @@ namespace Modumate {
 
 		FCraftingTreeNodeInstanceSharedPtr FCraftingTreeNodeInstancePool::CreateNodeInstanceFromDataRecord(const BIM::FCraftingPresetCollection &PresetCollection,const FCustomAssemblyCraftingNodeRecord &DataRecord)
 		{
-			FCraftingTreeNodeInstanceSharedPtr &instance = InstancePool.Add_GetRef(MakeShareable(new FCraftingTreeNodeInstance(DataRecord.InstanceID)));
+			FCraftingTreeNodeInstanceSharedPtr instance = InstancePool.Add_GetRef(MakeShareable(new FCraftingTreeNodeInstance(DataRecord.InstanceID)));
 
 			InstanceMap.Add(DataRecord.InstanceID, instance);
 			NextInstanceID = FMath::Max(NextInstanceID+1,DataRecord.InstanceID + 1);
@@ -852,7 +852,7 @@ namespace Modumate {
 				return nullptr;
 			}
 
-			FCraftingTreeNodeInstanceSharedPtr  &instance = InstancePool.Add_GetRef(MakeShareable(new FCraftingTreeNodeInstance(NextInstanceID)));
+			FCraftingTreeNodeInstanceSharedPtr instance = InstancePool.Add_GetRef(MakeShareable(new FCraftingTreeNodeInstance(NextInstanceID)));
 			InstanceMap.Add(NextInstanceID, instance);
 			++NextInstanceID;
 
