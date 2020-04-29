b701a44d42c8b38a0605e90382e7de73884c252f
Simon Walton <35795647+simon-walton@users.noreply.github.com>
2020-04-29 16:05:34 -0700

    Automated test for rendering performance (#66)
    * WIP - automated test to load a scene and profile rendering
    
    * Read stats from generated CSV
    
    * Fix project loading; other reviewer requests
    
    * Load via player-controller

---
+++ Modumate/Source/Modumate/Private/UnrealClasses/ModumateGraphicsTests.cpp
@@ -0,0 +1,151 @@
+// Copyright 2020 Modumate, Inc. All Rights Reserved.
+
+#include "Tests/AutomationCommon.h"
+#include "EditModelInputAutomation.h"
+#include "stats.h"
+#include "CsvProfiler.h"
+#include "Csv/CsvParser.h"
+#include "HAL/FileManager.h"
+#include "EditModelGameState_CPP.h"
+#include "EditModelPlayerController_CPP.h"
+#include "ModumateCommands.h"
+#include "ModumateGameInstance.h"
+
+
+namespace Modumate
+{
+#if WITH_DEV_AUTOMATION_TESTS
+
+	IMPLEMENT_SIMPLE_AUTOMATION_TEST(FGraphicsPerformanceTest1, "Modumate.Graphics.Performance",
+		EAutomationTestFlags::EditorContext | EAutomationTestFlags::ClientContext | EAutomationTestFlags::PerfFilter);
+
+	namespace {
+		FString sceneTestFilename("200310_BPHouse_06.mdmt");
+		TSharedFuture<FString> writeCsvFuture;
+
+		const float loadToCaptureTime = 2.0f;
+		const float captureDuration = 5.0f;
+		const TCHAR* requiredStats[] = { TEXT("FrameTime"), TEXT("InitViews_Scene"),  TEXT("InitViews_Shadows")};
+
+		DEFINE_LATENT_AUTOMATION_COMMAND(FGraphicsSceneLoaded);
+		bool FGraphicsSceneLoaded::Update()
+		{
+			const UWorld * currentWorld = GEngine->GetWorldContexts()[0].World();
+			const AEditModelGameState_CPP* gameState = currentWorld->GetGameState<AEditModelGameState_CPP>();
+			return !gameState->Document.CurrentProjectName.IsEmpty();
+		}
+
+		DEFINE_LATENT_AUTOMATION_COMMAND(FStartCapture);
+		bool FStartCapture::Update()
+		{
+			FCsvProfiler::Get()->BeginCapture();
+			return true;
+		}
+
+		DEFINE_LATENT_AUTOMATION_COMMAND(FReadStats);
+		bool FReadStats::Update()
+		{
+			FCsvProfiler* csvProfiler = FCsvProfiler::Get();
+			writeCsvFuture = csvProfiler->EndCapture();
+			return true;
+		}
+
+		DEFINE_LATENT_AUTOMATION_COMMAND(FProcessCsvFile);
+		bool FProcessCsvFile::Update()
+		{
+			if (writeCsvFuture.IsReady())
+			{
+				FString csvFilename = writeCsvFuture.Get();
+				writeCsvFuture = TSharedFuture<FString>();
+
+				FString csvContents;
+				if (FFileHelper::LoadFileToString(csvContents, *csvFilename))
+				{
+					FCsvParser csvParser(csvContents);
+					const FCsvParser::FRows contents = csvParser.GetRows();
+					const int tableSize = contents.Num() - 3;
+					if (tableSize > 0)
+					{
+						const TArray<const TCHAR*>& headers = contents[0];
+						for (int rs = 0; rs < sizeof(requiredStats) / sizeof(requiredStats[0]); ++rs)
+						{
+							const TCHAR* requiredStat = requiredStats[rs];
+							int requiredColumn = -1;
+							for (int c = 1; c < headers.Num(); ++c)
+							{
+								if (FCString::Stristr(headers[c], requiredStat))
+								{
+									requiredColumn = c;
+									break;
+								}
+							}
+							if (requiredColumn >= 0)
+							{
+								double total = 0.0;
+								for (int r = 0; r < tableSize; ++r)
+								{
+									total += FCString::Atod(contents[r + 1][requiredColumn]);
+								}
+								double stat = total / tableSize;
+								UE_LOG(LogPerformance, Display, TEXT("FGraphicsPerformanceTest1: %s = %lf ms"), requiredStat, stat);
+							}
+						}
+					}
+
+					IFileManager::Get().Delete(*csvFilename);
+				}
+				return true;
+			}
+			return false;
+		}
+
+		DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FGraphicsPerformanceLoadScene, FString, sceneFile);
+		bool FGraphicsPerformanceLoadScene::Update()
+		{
+			UWorld* world = nullptr;
+
+			for (const FWorldContext& worldContext : GEngine->GetWorldContexts())
+			{
+				if (worldContext.WorldType == EWorldType::Game)
+				{
+					world = worldContext.World();
+					break;
+				}
+			}
+
+			if (!world)
+			{
+				return false;
+			}
+
+			FString scenePathname =
+				FPaths::ProjectDir() / world->GetGameInstance<UModumateGameInstance>()->TestScriptRelativePath / sceneFile;
+			
+			AEditModelPlayerController_CPP * playerController = world->GetFirstPlayerController<AEditModelPlayerController_CPP>();
+			playerController->LoadModelFilePath(scenePathname, false);
+
+			return true;
+		}
+
+	} // Anonymous namespace
+
+
+	bool FGraphicsPerformanceTest1::RunTest(const FString& Parameters)
+	{
+		ADD_LATENT_AUTOMATION_COMMAND(FLoadGameMapCommand("EditModelLVL"));
+		ADD_LATENT_AUTOMATION_COMMAND(FEngineWaitLatentCommand(2.0f));
+		ADD_LATENT_AUTOMATION_COMMAND(FWaitForMapToLoadCommand());
+		ADD_LATENT_AUTOMATION_COMMAND(FExecStringLatentCommand(TEXT("setres 1600x1200")) );
+		ADD_LATENT_AUTOMATION_COMMAND(FGraphicsPerformanceLoadScene(sceneTestFilename));
+		ADD_LATENT_AUTOMATION_COMMAND(FGraphicsSceneLoaded);
+		ADD_LATENT_AUTOMATION_COMMAND(FEngineWaitLatentCommand(loadToCaptureTime));
+		ADD_LATENT_AUTOMATION_COMMAND(FStartCapture);
+		ADD_LATENT_AUTOMATION_COMMAND(FEngineWaitLatentCommand(captureDuration));
+		ADD_LATENT_AUTOMATION_COMMAND(FReadStats);
+		ADD_LATENT_AUTOMATION_COMMAND(FProcessCsvFile);
+
+		return true;
+	}
+
+#endif  // WITH_DEV_AUTOMATION_TESTS
+}
