7cdea7bc3a96c43f479e5c02a4f221d55f0fb29d
dboehle-modumate <62911378+dboehle-modumate@users.noreply.github.com>
2020-09-09 10:35:15 -0700

    Disable deprecated tests, and parse new format of test results (#376)

--- Modumate/Scripts/check_test_logs.py
+++ Modumate/Scripts/check_test_logs.py
@@ -1,54 +1,55 @@
-import datetime
+import json
 import os
-import re
 import shutil
 import sys
 
-log_pat = re.compile(r'.*LogAutomationController: .*Test Completed.*'
-                     'Result={(?P<result>\w+)}.*Name={(?P<shortname>.+)}.*Path={(?P<longname>.+)}')
-success_result = "Passed"
-
-def check_test_logs(log_path, build_id):
+def check_test_logs(results_path, build_id):
     passed_tests = []
     failed_tests = []
-    file_exists = False
+    timestamp = None
 
-    with open(log_path) as log_file:
-        file_exists = True
-        for log_line in log_file:
-            m = log_pat.match(log_line)
-            if m:
-                test_name = m.group('longname')
-                result = m.group('result')
+    try:
+        with open(results_path, encoding='utf-8-sig') as results_file:
+            results_obj = json.load(results_file)
 
-                if result == success_result:
+            timestamp = results_obj['reportCreatedOn']
+            for test in results_obj['tests']:
+                test_name = test['fullTestPath']
+                test_result = test['state']
+                num_errors = test['errors']
+                test_success = (test_result == 'Success') and (num_errors == 0)
+
+                if test_success:
                     passed_tests.append(test_name)
                 else:
                     failed_tests.append(test_name)
 
-                print("Test result for %s: %s" % (m.group('longname'), m.group('result')))
+                print("Test result for %s: %s" % (test_name, "success" if test_success else "failure"))
+    except e:
+        pass
 
-    if file_exists:
-        path_base, path_ext = os.path.splitext(log_path)
-        time_str = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
-        log_copy_path = "%s-build-%s-%s%s" % (path_base, build_id, time_str, path_ext)
-        shutil.copy(log_path, log_copy_path)
+    if timestamp is not None:
+        results_dir, results_filename = os.path.split(results_path)
+        results_filebase, results_fileext = os.path.splitext(results_filename)
+        results_copy_filename = "test-results_build-%s_%s%s" % (build_id, timestamp, results_fileext)
+        results_copy_path = os.path.join(results_dir, results_copy_filename)
+        shutil.copy(results_path, results_copy_path)
 
-    return passed_tests, failed_tests
+    num_passed_tests = len(passed_tests)
+    num_failed_tests = len(failed_tests)
+    if num_failed_tests > 0:
+        print("ERROR: %d test(s) failed!" % num_failed_tests)
+        return 1
+    elif num_passed_tests == 0:
+        print("ERROR: no tests found!")
+        return 2
+    else:
+        print("SUCCESS: all %d tests passed!" % num_passed_tests)
+        return 0
 
 if __name__ == '__main__':
     if len(sys.argv) > 2:
-        passed_tests, failed_tests = check_test_logs(sys.argv[1], sys.argv[2])
-        num_passed_tests = len(passed_tests)
-        num_failed_tests = len(failed_tests)
-
-        if num_failed_tests > 0:
-            print("ERROR: %d test(s) failed!" % num_failed_tests)
-            sys.exit(1)
-        elif num_passed_tests == 0:
-            print("ERROR: no tests found!")
-            sys.exit(2)
-        else:
-            print("SUCCESS: all %d tests passed!" % num_passed_tests)
+        sys.exit(check_test_logs(sys.argv[1], sys.argv[2]))
     else:
+        print("ERROR: must supply arguments for the path to the test results and the build ID!")
         sys.exit(-1)
