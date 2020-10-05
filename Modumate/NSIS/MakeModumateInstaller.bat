8e4f9e60db55ca266d4c7eecf621507a7f623569
JShankelModumate <63130365+JShankelModumate@users.noreply.github.com>
2020-10-05 10:25:29 -0700

    update version (#494)

--- Modumate/NSIS/MakeModumateInstaller.bat
+++ Modumate/NSIS/MakeModumateInstaller.bat
@@ -1,2 +1,2 @@
 REM TODO: receive product version, path_out_root and path_in from command line in Jenkins  
-"C:\Program Files (x86)\NSIS\makensis.exe" /DPRODUCT_VERSION="0.7.0.3" /DPATH_OUT_ROOT="c:\ReleaseBuilds" /DPATH_IN="c:\Modumate\Builds\Release\WindowsNoEditor" .\ModumateSetup.nsi
+"C:\Program Files (x86)\NSIS\makensis.exe" /DPRODUCT_VERSION="0.8.0.0" /DPATH_OUT_ROOT="c:\ReleaseBuilds" /DPATH_IN="c:\Modumate\Builds\Release\WindowsNoEditor" .\ModumateSetup.nsi
