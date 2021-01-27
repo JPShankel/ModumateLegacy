255c0a033203bc009332c22c6a179c96d5e3c792
jasonaisrael <62911800+jasonaisrael@users.noreply.github.com>
2021-01-26 17:55:56 -0800

    Feature/viewcube (#955)
    * initial viewcube work
    
    * wireframe cube
    
    * functioning buttons
    
    * expose properties to blueprint
    
    * Fixed corners of cube, positioning in parent widget, preview drawing
    
    Co-authored-by: dboehle-modumate <duncan.boehle@modumate.com>

---
+++ Modumate/Source/Modumate/Private/UI/ViewCubeWidget.cpp
@@ -0,0 +1,168 @@
+#include "UI/ViewCubeWidget.h"
+
+#include "Blueprint/WidgetBlueprintLibrary.h"
+#include "ModumateCore/ModumateGeometryStatics.h"
+#include "UnrealClasses/EditModelCameraController.h"
+#include "UnrealClasses/EditModelPlayerCameraManager.h"
+#include "UnrealClasses/EditModelPlayerController.h"
+#include "UnrealClasses/EditModelPlayerState.h"
+#include "Components/Button.h"
+
+UViewCubeWidget::UViewCubeWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+	, DefaultCameraForward(FVector::ForwardVector)
+{
+
+}
+
+bool UViewCubeWidget::Initialize()
+{
+	if (!Super::Initialize())
+	{
+		return false;
+	}
+
+	Controller = Cast<AEditModelPlayerController>(GetOwningPlayer());
+	CameraManager = Controller ? Controller->PlayerCameraManager : nullptr;
+
+	if (!(ButtonSnapUp && ButtonSnapDown && ButtonSnapLeft && ButtonSnapRight && ButtonSnapForward && ButtonSnapBackward))
+	{
+		return false;
+	}
+
+	ButtonSnapUp->OnReleased.AddDynamic(this, &UViewCubeWidget::OnButtonPressedSnapUp);
+	ButtonSnapDown->OnReleased.AddDynamic(this, &UViewCubeWidget::OnButtonPressedSnapDown);
+	ButtonSnapLeft->OnReleased.AddDynamic(this, &UViewCubeWidget::OnButtonPressedSnapLeft);
+	ButtonSnapRight->OnReleased.AddDynamic(this, &UViewCubeWidget::OnButtonPressedSnapRight);
+	ButtonSnapForward->OnReleased.AddDynamic(this, &UViewCubeWidget::OnButtonPressedSnapForward);
+	ButtonSnapBackward->OnReleased.AddDynamic(this, &UViewCubeWidget::OnButtonPressedSnapBackward);
+
+	DirectionToButton.Add(FVector::UpVector, ButtonSnapUp);
+	DirectionToButton.Add(FVector::DownVector, ButtonSnapDown);
+	DirectionToButton.Add(FVector::LeftVector, ButtonSnapLeft);
+	DirectionToButton.Add(FVector::RightVector, ButtonSnapRight);
+	DirectionToButton.Add(FVector::ForwardVector, ButtonSnapForward);
+	DirectionToButton.Add(FVector::BackwardVector, ButtonSnapBackward);
+
+	return true;
+}
+
+int32 UViewCubeWidget::NativePaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId, const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const
+{
+	int32 parentMaxLayerID = Super::NativePaint(Args, AllottedGeometry, MyCullingRect, OutDrawElements, LayerId, InWidgetStyle, bParentEnabled);
+	int32 selfPaintLayerID = LayerId;
+
+	FPaintContext selfPaintContext(AllottedGeometry, MyCullingRect, OutDrawElements, LayerId, InWidgetStyle, bParentEnabled);
+
+	FVector forwardVector = CameraManager ? CameraManager->GetActorForwardVector() : DefaultCameraForward;
+
+	for (auto& kvp : DirectionToButton)
+	{
+		auto& direction = kvp.Key;
+		auto* button = kvp.Value;
+
+		// only show lines and buttons for faces with normals that would face the camera
+		if ((forwardVector | direction) < THRESH_NORMALS_ARE_ORTHOGONAL)
+		{
+			button->SetVisibility(ESlateVisibility::Hidden);
+			continue;
+		}
+		button->SetVisibility(ESlateVisibility::Visible);
+
+		FVector axisX, axisY;
+		UModumateGeometryStatics::FindBasisVectors(axisX, axisY, direction);
+		TArray<FVector> points = {
+			direction + axisX + axisY,
+			direction + axisX - axisY,
+			direction - axisX - axisY,
+			direction - axisX + axisY
+		};
+
+		FWidgetTransform newTransform;
+		newTransform.Translation = TransformVector(direction * CubeSideLength);
+		newTransform.Angle = FMath::RadiansToDegrees(FMath::Atan2(newTransform.Translation.Y, newTransform.Translation.X));
+		button->SetRenderTransform(newTransform);
+
+		for (int32 i = 0; i < 4; i++)
+		{
+			FVector2D pointStart = TransformVector(CubeSideLength * points[i]);
+			FVector2D pointEnd = TransformVector(CubeSideLength * points[(i + 1) % 4]);
+			FVector2D lineDir = (pointEnd - pointStart).GetSafeNormal();
+			pointStart += 0.5f * LineThickness * lineDir;
+			pointEnd -= 0.5f * LineThickness * lineDir;
+
+			UWidgetBlueprintLibrary::DrawLine(selfPaintContext,
+				pointStart, pointEnd,
+				LineColor, true, LineThickness);
+		}
+	}
+
+	return FMath::Max(parentMaxLayerID, selfPaintLayerID);
+}
+
+void UViewCubeWidget::OnButtonPressedSnapUp()
+{
+	Zoom(FVector::UpVector, FVector::BackwardVector);
+}
+
+void UViewCubeWidget::OnButtonPressedSnapDown()
+{
+	Zoom(FVector::DownVector, FVector::ForwardVector);
+}
+
+void UViewCubeWidget::OnButtonPressedSnapLeft()
+{
+	Zoom(FVector::LeftVector, FVector::UpVector);
+}
+
+void UViewCubeWidget::OnButtonPressedSnapRight()
+{
+	Zoom(FVector::RightVector, FVector::UpVector);
+}
+
+void UViewCubeWidget::OnButtonPressedSnapForward()
+{
+	Zoom(FVector::ForwardVector, FVector::UpVector);
+}
+
+void UViewCubeWidget::OnButtonPressedSnapBackward()
+{
+	Zoom(FVector::BackwardVector, FVector::UpVector);
+}
+
+void UViewCubeWidget::Zoom(const FVector& InForward, const FVector& InUp)
+{
+	if (Controller == nullptr)
+	{
+		return;
+	}
+
+	if (Controller->EMPlayerState->SelectedObjects.Num() > 0)
+	{
+		Controller->CameraController->ZoomToSelection(InForward, InUp);
+	}
+	else
+	{
+		Controller->CameraController->ZoomToProjectExtents(InForward, InUp);
+	}
+}
+
+FVector2D UViewCubeWidget::TransformVector(const FVector& InVector) const
+{
+	FVector cameraRight, cameraUp;
+	if (CameraManager)
+	{
+		cameraRight = CameraManager->GetActorRightVector();
+		cameraUp = CameraManager->GetActorUpVector();
+	}
+	else
+	{
+		FQuat defaultCameraRot = FRotationMatrix::MakeFromX(DefaultCameraForward).ToQuat();
+		cameraRight = defaultCameraRot.GetRightVector();
+		cameraUp = defaultCameraRot.GetUpVector();
+	}
+
+	float x = (-cameraRight) | (InVector);
+	float y = cameraUp | (InVector);
+	return FVector2D(x, y);
+}
---
+++ Modumate/Source/Modumate/Public/UI/ViewCubeWidget.h
@@ -0,0 +1,81 @@
+// Copyright 2021 Modumate, Inc. All Rights Reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "ViewCubeWidget.generated.h"
+
+UCLASS()
+class MODUMATE_API UViewCubeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UViewCubeWidget(const FObjectInitializer& ObjectInitializer);
+	virtual bool Initialize() override;
+
+	virtual int32 NativePaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId, const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const override;
+
+	UFUNCTION()
+	void OnButtonPressedSnapUp();
+
+	UFUNCTION()
+	void OnButtonPressedSnapDown();
+
+	UFUNCTION()
+	void OnButtonPressedSnapLeft();
+
+	UFUNCTION()
+	void OnButtonPressedSnapRight();
+
+	UFUNCTION()
+	void OnButtonPressedSnapForward();
+
+	UFUNCTION()
+	void OnButtonPressedSnapBackward();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (BindWidget))
+	class UButton *ButtonSnapUp;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (BindWidget))
+	class UButton *ButtonSnapDown;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (BindWidget))
+	class UButton *ButtonSnapLeft;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (BindWidget))
+	class UButton *ButtonSnapRight;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (BindWidget))
+	class UButton *ButtonSnapForward;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (BindWidget))
+	class UButton *ButtonSnapBackward;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float LineThickness;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FColor LineColor;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float CubeSideLength;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FVector DefaultCameraForward;
+
+private:
+	void Zoom(const FVector& InForward, const FVector& InUp);
+	FVector2D TransformVector(const FVector& InVector) const;
+
+private:
+	UPROPERTY()
+	class AEditModelPlayerController* Controller;
+
+	UPROPERTY()
+	class APlayerCameraManager* CameraManager;
+
+	UPROPERTY()
+	TMap<FVector, class UButton*> DirectionToButton;
+};
