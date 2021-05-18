f69bb38bb8ade97e5c85b3a8ea83e600d95bfa40
JShankelModumate <63130365+JShankelModumate@users.noreply.github.com>
2021-05-17 17:30:11 -0700

    Mac/code signing (#1340)
    * entitlements
    
    * rpath update scripts
    
    * single rpath script
    
    * terminating ;
    
    * rpath parameter fix
    
    * add code signing
    
    * build and sign dmg file
    
    * find entitlements dir
    
    * fix entitlement path
    
    * remove 'Unconverted' from install name
    
    Co-authored-by: Duncan Boehle <duncan.boehle@modumate.com>

---
+++ Modumate/Scripts/build_dmg.sh
@@ -0,0 +1,6 @@
+rm /Users/buildy/Documents/$1/Modumate-Installer-Unconverted.dmg
+hdiutil create -volname Modumate-Installer -srcfolder /Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app -ov -format UDRO /Users/buildy/Documents/$1/Modumate-Installer-Unconverted.dmg
+rm /Users/buildy/Documents/$1/Modumate-Installer.dmg
+hdiutil convert /Users/buildy/Documents/$1/Modumate-Installer-Unconverted.dmg -format UDRO -o /Users/buildy/Documents/$1/Modumate-Installer.dmg
+rm /Users/buildy/Documents/$1/Modumate-Installer-Unconverted.dmg
+codesign  -s "Developer ID Application: Modumate, Inc. (44CC46TSLW)"  --timestamp -i "com.Modumate.Modumate" "/Users/buildy/Documents/$1/Modumate-Installer.dmg"
