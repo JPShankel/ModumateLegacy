6a247c6d89df3ec13333463de53b4091a37cb0fb
JShankelModumate <63130365+JShankelModumate@users.noreply.github.com>
2021-05-18 17:17:30 -0700

    Mac/code signing (#1345)
    * rpath update scripts
    
    * single rpath script
    
    * terminating ;
    
    * rpath parameter fix
    
    * add code signing
    
    * build and sign dmg file
    
    * find entitlements dir
    
    * fix entitlement path
    
    * remove 'Unconverted' from install name
    
    * remove radio component for notarization
    
    * sign app binary not folder
    
    * sign crash reporter first
    
    Co-authored-by: Duncan Boehle <duncan.boehle@modumate.com>

--- Modumate/Scripts/update_rpaths.sh
+++ Modumate/Scripts/update_rpaths.sh
@@ -1,4 +1,6 @@
+rm -r /Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents/UE4/Engine/Build
+rm -r /Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents/Resources/RadioEffectUnit.component
 find /Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents -type f '(' -name '*.dylib' ')' -exec sh -c 'install_name_tool -add_rpath $(dirname "$1") "$1"' sh {} \;
 find /Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents | grep .dylib | xargs codesign -f -v -s "Developer ID Application: Modumate, Inc. (44CC46TSLW)" --entitlements "./Modumate/Scripts/entitlements.plist" --options runtime --timestamp
-codesign -f -v -s "Developer ID Application: Modumate, Inc. (44CC46TSLW)" --entitlements "./Modumate/Scripts/entitlements.plist" --options runtime --timestamp "/Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app"
 codesign -f -v -s "Developer ID Application: Modumate, Inc. (44CC46TSLW)" --entitlements "./Modumate/Scripts/entitlements.plist" --options runtime --timestamp "/Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents/UE4/Engine/Binaries/Mac/CrashReportClient.app"
+codesign -f -v -s "Developer ID Application: Modumate, Inc. (44CC46TSLW)" --entitlements "./Modumate/Scripts/entitlements.plist" --options runtime --timestamp "/Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app"
