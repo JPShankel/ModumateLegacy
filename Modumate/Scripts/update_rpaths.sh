d3b384a5273ba555de96def32dd9a1c44045b05e
JShankelModumate <63130365+JShankelModumate@users.noreply.github.com>
2021-09-13 17:49:32 -0700

    Mac/jenkins fix (#1586)
    * set version and attach to production server
    
    * certificates and provisioning
    
    * chmod installer dir
    
    * fix output folder
    
    * don't forget read access
    
    * chmod directory itself
    
    * roll to 2.2.0.0
    
    * remove mac-only for branch

--- Modumate/Scripts/update_rpaths.sh
+++ Modumate/Scripts/update_rpaths.sh
@@ -1,13 +1,14 @@
+chmod -R a+rw "/Users/buildy/Documents/"$1
 infoplist="/Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents/Info.plist"
 sedcom="sed -I b 's/"$2"/"$1"/' $infoplist"
 eval "$sedcom"
 infoplist="${infoplist}b"
 rm $infoplist
+cp ./Modumate/Scripts/Modumate_Mac_Client.provisionprofile ~/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents/embedded.provisionprofile
 rm -r /Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents/UE4/Engine/Build
 rm -r /Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents/Resources/RadioEffectUnit.component
 find /Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents -type f '(' -name '*.dylib' ')' -exec sh -c 'install_name_tool -add_rpath $(dirname "$1") "$1"' sh {} \;
 find /Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents | grep .dylib | xargs codesign -f -v -s "Developer ID Application: Modumate, Inc. (44CC46TSLW)" --options runtime --timestamp
-#find /Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents | grep .framework | xargs codesign -f -v -s "Developer ID Application: Modumate, Inc. (44CC46TSLW)" --options runtime --timestamp
 codesign -f -v -s "Developer ID Application: Modumate, Inc. (44CC46TSLW)" --entitlements "./Modumate/Scripts/entitlements.plist" --options runtime --timestamp "/Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents/UE4/Engine/Binaries/Mac/UnrealCEFSubProcess.app"
 codesign -f -v -s "Developer ID Application: Modumate, Inc. (44CC46TSLW)" --entitlements "./Modumate/Scripts/entitlements.plist" --options runtime --timestamp "/Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents/UE4/Engine/Binaries/ThirdParty/CEF3/Mac/Chromium Embedded Framework.framework"
 codesign -f -v -s "Developer ID Application: Modumate, Inc. (44CC46TSLW)" --entitlements "./Modumate/Scripts/entitlements.plist" --options runtime --timestamp "/Users/buildy/Documents/$1/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app/Contents/UE4/Engine/Binaries/Mac/CrashReportClient.app"
