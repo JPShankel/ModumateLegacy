b21e128ce1d12ff449c206beadbb04f9ec8cd213
JShankelModumate <63130365+JShankelModumate@users.noreply.github.com>
2022-05-06 14:31:30 -0700

    Singleton spans (#2014)
    * winding test
    
    * if 0 for test direction
    
    * WIP edge order for cached face
    
    * force spans to be singleton until we can fix adjustment and geometry
    
    * remove samedir check
    
    Co-authored-by: WesleyCheng <wesley.cheng@modumate.com>

--- Modumate/Source/Modumate/Private/Objects/MetaPlaneSpan.cpp
+++ Modumate/Source/Modumate/Private/Objects/MetaPlaneSpan.cpp
@@ -74,13 +74,30 @@ void AMOIMetaPlaneSpan::PreDestroy()
 	Super::PreDestroy();
 }
 
+#define ENFORCE_SINGLETON_SPANS
+
 bool AMOIMetaPlaneSpan::CleanObject(EObjectDirtyFlags DirtyFlag, TArray<FDeltaPtr>* OutSideEffectDeltas)
 {
 	if (DirtyFlag == EObjectDirtyFlags::Structure)
 	{
 		//UE_LOG(LogTemp, Display, TEXT("Cleaning span %d with %d members"),ID,InstanceData.GraphMembers.Num());
 
+#ifdef ENFORCE_SINGLETON_SPANS
+		if (InstanceData.GraphMembers.Num() > 1)
+		{
+			if (OutSideEffectDeltas != nullptr)
+			{
+				FModumateObjectDeltaStatics::GetDeltasForSpanSplit(Document, { ID }, *OutSideEffectDeltas);
+			}
+		}
+		else
+		{
+			UpdateCachedPerimeterFace();
+		}
+		return true;
+#endif
 		TArray<FDeltaPtr> outDeltas;
+
 		if (PostGraphChanges.Num() > 0)
 		{
 			TArray<int32> additions, removals;
@@ -165,12 +182,26 @@ bool AMOIMetaPlaneSpan::UpdateCachedPerimeterFace()
 	Algo::Transform(InstanceData.GraphMembers, memberFaces,
 		[graph](int32 FaceID) {return graph->FindFace(FaceID); });
 
-	if (memberFaces.Num() == 0)
-	{
-		return false;
-	}
-
 	FPlane basePlane = memberFaces[0]->CachedPlane;
+
+#ifdef ENFORCE_SINGLETON_SPANS
+	CachedPerimeterFace = *memberFaces[0];
+
+	// Update AMOIPlaneBase cached values
+	CachedPoints = CachedPerimeterFace.CachedPositions;
+	CachedPlane = CachedPerimeterFace.CachedPlane;
+	CachedAxisX = CachedPerimeterFace.Cached2DX;
+	CachedAxisY = CachedPerimeterFace.Cached2DY;
+	CachedOrigin = CachedPerimeterFace.CachedPositions.Num() > 0 ? CachedPerimeterFace.CachedPositions[0] : FVector::ZeroVector;
+	CachedCenter = CachedPerimeterFace.CachedCenter;
+	CachedHoles = CachedPerimeterFace.CachedHoles;
+	CachedEdgeIDs = TSet<int32>(CachedPerimeterFace.EdgeIDs);
+
+	Document->GetObjectById(graph->GraphID)->MarkDirty(EObjectDirtyFlags::Structure);
+
+	return true;
+
+#endif
 
 	auto numFacesForEdge = [this, &memberFaces](int32 EdgeID)
 	{
@@ -186,10 +217,8 @@ bool AMOIMetaPlaneSpan::UpdateCachedPerimeterFace()
 		return numFaces;
 	};
 
-
 	// Gather all the perimeter edges into an unsorted list
 	TArray<int32> perimeterEdges;
-	CachedEdgeIDs.Empty();
 	for (auto* face: memberFaces)
 	{
 		if (!face)
@@ -199,7 +228,6 @@ bool AMOIMetaPlaneSpan::UpdateCachedPerimeterFace()
 		// Edges are perimeter if they have only one face in the member list
 		for (auto edgeID : face->EdgeIDs)
 		{
-			CachedEdgeIDs.Add(edgeID);
 			if (numFacesForEdge(edgeID) == 1)
 			{
 				perimeterEdges.Add(edgeID);
@@ -244,7 +272,7 @@ bool AMOIMetaPlaneSpan::UpdateCachedPerimeterFace()
 	// When we hit a duplicate, the perimeter is done
 	// Note: if we ever need to support spans with detached faces, we'll need a new approach
 
-	int32 edgeID = perimeterEdges.Pop();
+	int32 edgeID = perimeterEdges[0];
 
 	//The cached perimeter face now has edges laid out in order
 	while (edgeID != 0)
@@ -264,12 +292,29 @@ bool AMOIMetaPlaneSpan::UpdateCachedPerimeterFace()
 
 		CachedPerimeterFace.VertexIDs.Add(vertID);
 		CachedPerimeterFace.CachedPositions.Add(graph->FindVertex(vertID)->Position);
-		CachedPerimeterFace.CachedEdgeNormals.Add(edgeOb->CachedRefNorm);
 	}
 
 	// And analyze the derived values
 	UModumateGeometryStatics::AnalyzeCachedPositions(CachedPerimeterFace.CachedPositions, CachedPerimeterFace.CachedPlane,
 		CachedPerimeterFace.Cached2DX, CachedPerimeterFace.Cached2DY, CachedPerimeterFace.Cached2DPositions, CachedPerimeterFace.CachedCenter, true);
+
+	for (auto perimeterEdge : CachedPerimeterFace.EdgeIDs)
+	{
+		const FGraph3DEdge* edgeOb = graph->FindEdge(perimeterEdge);
+		// Negative edge IDs indicate reverse direction
+		int32 vertID = perimeterEdge > 0 ? edgeOb->StartVertexID : edgeOb->EndVertexID;
+		const FGraph3DVertex* vertexOb = graph->FindVertex(vertID);
+
+		FVector edgeDir = edgeOb->CachedDir*(edgeOb->ID > 0 ? 1.0f : -1.0f);
+		FVector edgeNormal = CachedPlane ^ edgeDir;
+		edgeNormal.Normalize();
+		CachedPerimeterFace.CachedEdgeNormals.Add(edgeNormal);
+
+		// Also cache the 2D edge normal here, which should already be safe to project into the plane and stay normalized.
+		FVector2D edgeNormal2D = UModumateGeometryStatics::ProjectVector2D(edgeNormal, CachedPerimeterFace.Cached2DX, CachedPerimeterFace.Cached2DY);
+		bool edgeNormal2DNormalized = (FMath::Abs(1.f - edgeNormal2D.SizeSquared()) < THRESH_VECTOR_NORMALIZED);
+		CachedPerimeterFace.Cached2DEdgeNormals.Add(edgeNormal2D);
+	}
 
 	// Update AMOIPlaneBase cached values
 	CachedPoints = CachedPerimeterFace.CachedPositions;
@@ -279,10 +324,12 @@ bool AMOIMetaPlaneSpan::UpdateCachedPerimeterFace()
 	CachedOrigin = CachedPerimeterFace.CachedPositions.Num() > 0 ? CachedPerimeterFace.CachedPositions[0] : FVector::ZeroVector;
 	CachedCenter = CachedPerimeterFace.CachedCenter;
 	CachedHoles = CachedPerimeterFace.CachedHoles;
+	CachedEdgeIDs = TSet<int32>(CachedPerimeterFace.EdgeIDs);
 
 	Document->GetObjectById(graph->GraphID)->MarkDirty(EObjectDirtyFlags::Structure);
 
 	return bLegal && CheckIsConnected();
+
 }
 
 bool AMOIMetaPlaneSpan::CheckIsConnected() const
