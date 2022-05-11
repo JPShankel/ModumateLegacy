940739c3ed26fd928acfba9110ceda01ca426943
Joseph Nouri <87992111+joseph-nouri@users.noreply.github.com>
2022-05-11 12:23:22 -0600

    Check to see if Identity is set before signing (#2030)

--- Modumate/Scripts/CustomPackage.sh
+++ Modumate/Scripts/CustomPackage.sh
@@ -2,6 +2,9 @@
 
 oldDir=$(pwd)
 cd ../../
+
+GitDirectory=$(pwd)
+
 python3 Modumate/Scripts/utils.py get_project_version > ~/Documents/BuildVersion.txt
 python3 Modumate/Scripts/utils.py get_engine_version > ~/Documents/EngineVersion.txt
 export BUILD_VERSION=`cat ~/Documents/BuildVersion.txt`
@@ -11,10 +14,15 @@ export ENGINE_VERSION=`cat ~/Documents/EngineVersion.txt`
 ./UE4/Engine/Build/BatchFiles/Mac/Build.sh UnrealHeaderTool Mac Development -clean
 ./UE4/Engine/Build/BatchFiles/Mac/Build.sh UnrealHeaderTool Mac Shipping -clean
 
-./UE4/Engine/Build/BatchFiles/RunUAT.sh -ScriptsForProject=/Users/nourij/Code/ModumateGit/Modumate/Modumate.uproject BuildCookRun -nop4 -project=/Users/nourij/Code/ModumateGit/Modumate/Modumate.uproject -nocompilededitor -cook -stage -archive -archivedirectory=/Users/nourij/Documents/$BUILD_VERSION/ModumatePackaged -package -ue4exe=/Users/nourij/Code/ModumateGit/UE4/Engine/Binaries/Mac/UE4Editor.app/Contents/MacOS/UE4Editor -compressed -ddc=DerivedDataBackendGraph -pak -preeqs -distribution -nodebuginfo -manifests -platform=Mac -build -CrashReporter -architecture=x86_64 -target=Modumate -clientconfig=Shipping -utf8output
+#Because we cleaned the editor, we now have to rebuild it
+./UE4/Engine/Build/BatchFiles/Mac/Build.sh ModumateEditor Mac Development -WaitMutex
 
+#build the project
+./UE4/Engine/Build/BatchFiles/RunUAT.sh -ScriptsForProject=$GitDirectory/Modumate/Modumate.uproject BuildCookRun -nop4 -project=$GitDirectory/Modumate/Modumate.uproject -cook -stage -archive -archivedirectory=~/Documents/$BUILD_VERSION/ModumatePackaged -package -ue4exe=$GitDirectory/UE4/Engine/Binaries/Mac/UE4Editor.app/Contents/MacOS/UE4Editor -nocompilededitor -compressed -ddc=DerivedDataBackendGraph -pak -preeqs -distribution -nodebuginfo -manifests -platform=Mac -build -CrashReporter -architecture=x86_64 -target=Modumate -clientconfig=Shipping -utf8output
+
+#if it all succeeds, then we have to run the post build on the packaged app
 if [ $? -eq 0 ]; then
-    ./Modumate/Scripts/MacPostBuild.sh /Users/nourij/Code/ModumateGit/Modumate Modumate Shipping /Users/nourij/Documents/$BUILD_VERSION/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app
+    ./Modumate/Scripts/MacPostBuild.sh $GitDirectory/Modumate Modumate Shipping ~/Documents/$BUILD_VERSION/ModumatePackaged/MacNoEditor/Modumate-Mac-Shipping.app
 fi
 
 cd $oldDir
--- Modumate/Scripts/MacPostBuild.sh
+++ Modumate/Scripts/MacPostBuild.sh
@@ -101,45 +101,49 @@ if [[ ! -z "$BUILD_VERSION" ]]; then
             sedcom="sed -I b 's/"${ENGINE_VERSION}"/"${BUILD_VERSION}"/' ${INFO_PLIST}"
             echo $sedcom
             eval "$sedcom"
-            cp "${MODUMATE_GIT_PATH}/Modumate/Scripts/Modumate_Mac_Client.provisionprofile" "${UE_OUTPUT_APP}/Contents/embedded.provisionprofile"
             #sed creates a backup on mac no matter the args, don't know why -- but it needs to be deleted
             # prior to signing
             rm "${INFO_PLIST}b"
+            cp "${MODUMATE_GIT_PATH}/Modumate/Scripts/Modumate_Mac_Client.provisionprofile" "${UE_OUTPUT_APP}/Contents/embedded.provisionprofile"
         fi
     fi
 fi
 
 #-------------------------------------
 # Sign everything
+if [[ ! -z "$IDENTITY" ]]; then
 
-BUNDLE_GPU_APP="${UE_OUTPUT_APP}/Contents/Frameworks/UnrealCEFSubProcess (GPU).app"
-BUNDLE_RENDERER_APP="${UE_OUTPUT_APP}/Contents/Frameworks/UnrealCEFSubProcess (Renderer).app"
-BUNDLE_HELPER_APP="${UE_OUTPUT_APP}/Contents/Frameworks/UnrealCEFSubProcess.app"
-BUNDLE_FRAMEWORK="${UE_OUTPUT_APP}/Contents/Frameworks/Chromium Embedded Framework.framework"
+    BUNDLE_GPU_APP="${UE_OUTPUT_APP}/Contents/Frameworks/UnrealCEFSubProcess (GPU).app"
+    BUNDLE_RENDERER_APP="${UE_OUTPUT_APP}/Contents/Frameworks/UnrealCEFSubProcess (Renderer).app"
+    BUNDLE_HELPER_APP="${UE_OUTPUT_APP}/Contents/Frameworks/UnrealCEFSubProcess.app"
+    BUNDLE_FRAMEWORK="${UE_OUTPUT_APP}/Contents/Frameworks/Chromium Embedded Framework.framework"
 
-ENTITLEMENTS_UE="${MODUMATE_GIT_PATH}/Modumate/Scripts/ent_ue4editor.plist"
-ENTITLEMENTS_HELPER="${MODUMATE_GIT_PATH}/Modumate/Scripts/ent_cef_helper.plist"
-ENTITLEMENTS_FRAMEWORK="${MODUMATE_GIT_PATH}/Modumate/Scripts/ent_cef_helper.plist"
+    ENTITLEMENTS_UE="${MODUMATE_GIT_PATH}/Modumate/Scripts/ent_ue4editor.plist"
+    ENTITLEMENTS_HELPER="${MODUMATE_GIT_PATH}/Modumate/Scripts/ent_cef_helper.plist"
+    ENTITLEMENTS_FRAMEWORK="${MODUMATE_GIT_PATH}/Modumate/Scripts/ent_cef_helper.plist"
 
-echo "${highlight}Signing dylibs${normal}"
-find "${UE_OUTPUT_APP}/Contents" -name '*.dylib' -print0 | xargs -0 -J % codesign -f -v -s "$IDENTITY" --options runtime --timestamp "%"
+    echo "${highlight}Signing dylibs${normal}"
+    find "${UE_OUTPUT_APP}/Contents" -name '*.dylib' -print0 | xargs -0 -J % codesign -f -v -s "$IDENTITY" --options runtime --timestamp "%"
 
-echo "${highlight}Signing CEF Framework${normal}"
-codesign --force --options runtime --entitlements "$ENTITLEMENTS_FRAMEWORK" --sign "$IDENTITY" --timestamp --verbose "${BUNDLE_FRAMEWORK}"
+    echo "${highlight}Signing CEF Framework${normal}"
+    codesign --force --options runtime --entitlements "$ENTITLEMENTS_FRAMEWORK" --sign "$IDENTITY" --timestamp --verbose "${BUNDLE_FRAMEWORK}"
 
-echo "${highlight}Signing CrashReportClient.app${normal}"
-codesign --force --options runtime --entitlements "$ENTITLEMENTS_UE" --sign "$IDENTITY" --timestamp --verbose "${UE_OUTPUT_APP}/Contents/UE4/Engine/Binaries/Mac/CrashReportClient.app"
+    echo "${highlight}Signing CrashReportClient.app${normal}"
+    codesign --force --options runtime --entitlements "$ENTITLEMENTS_UE" --sign "$IDENTITY" --timestamp --verbose "${UE_OUTPUT_APP}/Contents/UE4/Engine/Binaries/Mac/CrashReportClient.app"
 
-echo "${highlight}Signing UnrealCEFSubProcess.app${normal}"
-codesign --force --options runtime --entitlements "$ENTITLEMENTS_HELPER" --sign "$IDENTITY" --timestamp --verbose "${BUNDLE_HELPER_APP}"
+    echo "${highlight}Signing UnrealCEFSubProcess.app${normal}"
+    codesign --force --options runtime --entitlements "$ENTITLEMENTS_HELPER" --sign "$IDENTITY" --timestamp --verbose "${BUNDLE_HELPER_APP}"
 
-echo "${highlight}Signing UnrealCEFSubProcess (GPU).app${normal}"
-codesign --force --options runtime --entitlements "$ENTITLEMENTS_HELPER" --sign "$IDENTITY" --timestamp --verbose "${BUNDLE_GPU_APP}"
+    echo "${highlight}Signing UnrealCEFSubProcess (GPU).app${normal}"
+    codesign --force --options runtime --entitlements "$ENTITLEMENTS_HELPER" --sign "$IDENTITY" --timestamp --verbose "${BUNDLE_GPU_APP}"
 
-echo "${highlight}Signing UnrealCEFSubProcess (Renderer).app${normal}"
-codesign --force --options runtime --entitlements "$ENTITLEMENTS_HELPER" --sign "$IDENTITY" --timestamp --verbose "${BUNDLE_RENDERER_APP}"
+    echo "${highlight}Signing UnrealCEFSubProcess (Renderer).app${normal}"
+    codesign --force --options runtime --entitlements "$ENTITLEMENTS_HELPER" --sign "$IDENTITY" --timestamp --verbose "${BUNDLE_RENDERER_APP}"
 
-echo "${highlight}Signing ${UE_OUTPUT_APP}${normal}"
-codesign --force --options runtime --entitlements "$ENTITLEMENTS_UE" --sign "$IDENTITY" --timestamp --verbose "${UE_OUTPUT_APP}"
+    echo "${highlight}Signing ${UE_OUTPUT_APP}${normal}"
+    codesign --force --options runtime --entitlements "$ENTITLEMENTS_UE" --sign "$IDENTITY" --timestamp --verbose "${UE_OUTPUT_APP}"
 
+else
+    echo "${highlight}Skipping signing (no identity found)${normal}"
+fi
 cd $oldDir
